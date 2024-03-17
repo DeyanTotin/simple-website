475000f71191368fc172c8137580fe57c95ed627
DeyanTotin <138721028+DeyanTotin@users.noreply.github.com>
2024-03-18 00:03:34 +0200

    Added aboutme.html and added a functinality to the nav-bar

--- jsUtilities/scripts.js
+++ jsUtilities/scripts.js
@@ -2,13 +2,29 @@
 
 
 document.addEventListener('DOMContentLoaded', function () {
-    var birthday = new Date('2004/10/25');
-    var today = new Date();
-    var age = today.getFullYear() - birthday.getFullYear();
-    var m = today.getMonth() - birthday.getMonth();
+    let birthday = new Date('2004/10/25');
+    let today = new Date();
+    let age = today.getFullYear() - birthday.getFullYear();
+    let m = today.getMonth() - birthday.getMonth();
     if (m < 0 || (m === 0 && today.getDate() < birthday.getDate())) {
         age--;
     }
     document.getElementById('age').textContent = age;
 });
 
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    let links = document.querySelectorAll('.nav-link');
+
+    links.forEach(link => {
+        link.addEventListener('click', function (e) {
+            e.preventDefault();
+            let href = this.getAttribute('href');
+            let element = document.querySelector(href);
+            window.location.href = href;
+
+        }());
+    });
+
+});
