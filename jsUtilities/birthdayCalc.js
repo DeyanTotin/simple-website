cf7e834af1f71417cb0f4658f6a078c12d4f6f88
DeyanTotin <138721028+DeyanTotin@users.noreply.github.com>
2024-03-17 18:38:22 +0200

    first commit

---
+++ jsUtilities/birthdayCalc.js
@@ -0,0 +1,13 @@
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var birthday = new Date('2004/10/25');
+    var today = new Date();
+    var age = today.getFullYear() - birthday.getFullYear();
+    var m = today.getMonth() - birthday.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthday.getDate())) {
+        age--;
+    }
+    document.getElementById('age').textContent = age;
+});
