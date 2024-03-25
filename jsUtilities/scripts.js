630102f8c85ef0be1c49e971f5e372c2ca435d72
DeyanTotin <138721028+DeyanTotin@users.noreply.github.com>
2024-03-26 01:08:56 +0200

    Added section for direct access to github projects, made script to get the newest 3 project from my github

--- jsUtilities/scripts.js
+++ jsUtilities/scripts.js
@@ -11,3 +11,39 @@ document.addEventListener('DOMContentLoaded', function () {
     }
     document.getElementById('age').textContent = age;
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    fetchProjects();
+});
+
+function fetchProjects() {
+    fetch('https://api.github.com/users/DeyanTotin/repos')
+    .then(response => response.json())
+    .then(data => {
+        displayProjects(data);
+    })
+    .catch(error => console.error('Error fetching data: ', error));
+}
+
+function displayProjects(repos) {
+    const projectsContainer = document.getElementById('projects-container');
+    
+    repos.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
+    
+    const newestRepos = repos.slice(0, 3);
+
+    newestRepos.forEach(repo => {
+
+        const projectElement = document.createElement('div');
+        projectElement.classList.add('project');
+        
+        const projectHTML = `
+            <h3>${repo.name}</h3>
+            <p>${repo.description || 'No description available'}</p>
+            <a href="${repo.html_url}" target="_blank">View on GitHub</a>
+        `;
+        projectElement.innerHTML = projectHTML;
+        projectsContainer.appendChild(projectElement);
+    });
+}
